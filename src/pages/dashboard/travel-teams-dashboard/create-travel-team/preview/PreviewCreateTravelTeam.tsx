6dacb676c836a1efeea0ff5a24e7979802aa3fcf
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-05-30 12:59:04 -0400

    Made travel team card into re usable component

--- src/pages/dashboard/travel-teams-dashboard/create-travel-team/preview/PreviewCreateTravelTeam.tsx
+++ src/pages/dashboard/travel-teams-dashboard/create-travel-team/preview/PreviewCreateTravelTeam.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './PreviewCreateTravelTeam.css'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
+import TravelTeamCard from './TravelTeamCard';
+
 
 
 
@@ -43,41 +45,9 @@ const PreviewCreateTravelTeam = () => {
                     <a className="nav-link tab active" href="#">Preview</a>
                 </li>
             </ul>
-            <div className="card d-grid mb-3 overflow-hidden shadow-sm" >
-                <div className="row g-0">
 
-                    <div className="col-md-8" >
-                        <div className="card-body" >
-
-                            <h2 className="card-title mb-1"><strong>{name}</strong></h2>
-                            <span style={{ lineHeight: '40px' }} >
-                                <i className="bi bi-calendar d-inline " style={{ paddingRight: '10px' }}></i>
-                                <p className='d-inline '>
-                                    {startDate}
-                                    <i className="bi bi-arrow-right-short"></i>
-                                    {endDate}
-                                </p>
-                            </span>
-                            <br />
-                            <span >
-                                <i className="bi bi-geo-alt-fill d-inline" style={{ paddingRight: '10px' }}></i>
-                                <p className='d-inline' >
-                                    {location}
-                                </p>
-                            </span>
-
-                            <p className="card-text mb-3 mt-1" dangerouslySetInnerHTML={{ __html: rules }}></p>
-                            <a href="#" className="btn btn-primary w-100">Register your child here</a>
-                        </div>
-
-                    </div>
-                    <div className="col-md-4">
-                        <img src="/src/assets/tournaments.jpg" className="img-fluid h-100 object-fit-cover  " alt="..." />
-                        {/* <p className="card-text"><small className="text-body-secondary position-absolute bottom-0 end-0">Posted 3 mins ago</small></p> */}
-
-                    </div>
-                </div>
-            </div>
+            <TravelTeamCard name={name} location={location} startDate={startDate} endDate={endDate} rules={rules} />
+            
             <Link to='../travel-teams'>
                 <button type="button" className="btn btn-labeled btn-danger ">
                     <span className="btn-label"><i className="bi bi-x"></i></span>
