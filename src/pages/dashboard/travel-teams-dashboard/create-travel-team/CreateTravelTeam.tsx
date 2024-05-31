dc5b9c80e1d499b50ce6b6b1102a18b2ee07ddc4
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-05-30 23:21:25 -0400

    Made the editor into a component for more reusable code

--- src/pages/dashboard/travel-teams-dashboard/create-travel-team/CreateTravelTeam.tsx
+++ src/pages/dashboard/travel-teams-dashboard/create-travel-team/CreateTravelTeam.tsx
@@ -21,6 +21,7 @@ import classNames from 'classnames'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import BulletList from '@tiptap/extension-bullet-list';
 import OrderedList from '@tiptap/extension-ordered-list';
+import EditorComponent from './EditorComponent';
 
 
 
@@ -39,6 +40,23 @@ const CreateTravelTeam = () => {
     `);
 
     const navigate = useNavigate();
+
+    function preview(event: any) {
+        event.preventDefault();
+
+        navigate("/dashboard/travel-teams/create/preview",
+            {
+                state: {
+                    name,
+                    location,
+                    startDate,
+                    endDate,
+                    rules
+                }
+            }
+        );
+
+    }
 
     const editor = useEditor({
         extensions: [
@@ -71,73 +89,14 @@ const CreateTravelTeam = () => {
 
             setRules(state.rules)
             editor?.commands.setContent(state.rules);
-            
+
         } catch (error) {
         }
     }, [editor]);
 
-    const toggleBold = useCallback(() => {
-        editor.chain().focus().toggleBold().run();
-    }, [editor]);
-
-    const toggleUnderline = useCallback(() => {
-        editor.chain().focus().toggleUnderline().run();
-    }, [editor]);
-
-    const toggleItalic = useCallback(() => {
-        editor.chain().focus().toggleItalic().run();
-    }, [editor]);
-
-    const toggleStrike = useCallback(() => {
-        editor.chain().focus().toggleStrike().run();
-    }, [editor]);
-
-    const toggleParagraph = useCallback(() => {
-        editor.chain().focus().setParagraph().run();
-    }, [editor]);
-
-    const toggleBulletList = useCallback(() => {
-        editor.chain().focus().toggleBulletList().run();
-    }, [editor]);
-
-    const toggleOrderedList = useCallback(() => {
-        editor.chain().focus().toggleOrderedList().run();
-    }, [editor]);
-
-    const toggleH1 = useCallback(() => {
-        editor.chain().focus().toggleHeading({ level: 1 }).run();
-    }, [editor]);
-
-    const toggleH2 = useCallback(() => {
-        editor.chain().focus().toggleHeading({ level: 2 }).run();
-    }, [editor]);
-    const toggleH3 = useCallback(() => {
-        editor.chain().focus().toggleHeading({ level: 3 }).run();
-    }, [editor]);
 
     if (!editor) {
         return null;
-    }
-
-
-
-
-
-    function preview(event: any) {
-        event.preventDefault();
-
-        navigate("/dashboard/travel-teams/create/preview",
-            {
-                state: {
-                    name,
-                    location,
-                    startDate,
-                    endDate,
-                    rules
-                }
-            }
-        );
-
     }
 
     return (
@@ -176,107 +135,7 @@ const CreateTravelTeam = () => {
                 </div>
 
                 <p className='form-label fs-5'>Rules</p>
-                <div className="editor mb-4">
-                    <div className="menu">
-                        <button
-                            className="menu-button"
-                            onClick={() => editor.chain().focus().undo().run()}
-                            disabled={!editor.can().undo()}
-                        >
-                            <Icons.RotateLeft />
-                        </button>
-                        <button
-                            className="menu-button"
-                            onClick={() => editor.chain().focus().redo().run()}
-                            disabled={!editor.can().redo()}
-                        >
-                            <Icons.RotateRight />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("parapgraph")
-                            })}
-                            onClick={toggleParagraph}
-                        >
-                            <Icons.Paragraph />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("H1")
-                            })}
-                            onClick={toggleH1}
-                        >
-                            <Icons.H1 />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("H2")
-                            })}
-                            onClick={toggleH2}
-                        >
-                            <Icons.H2 />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("H3")
-                            })}
-                            onClick={toggleH3}
-                        >
-                            <Icons.H3 />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("ul")
-                            })}
-                            onClick={toggleBulletList}
-                        >
-                            <Icons.BulletList />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("ol")
-                            })}
-                            onClick={toggleOrderedList}
-                        >
-                            <Icons.OrderedList />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("bold")
-                            })}
-                            onClick={toggleBold}
-                        >
-                            <Icons.Bold />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("underline")
-                            })}
-                            onClick={toggleUnderline}
-                        >
-                            <Icons.Underline />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("intalic")
-                            })}
-                            onClick={toggleItalic}
-                        >
-                            <Icons.Italic />
-                        </button>
-                        <button
-                            className={classNames("menu-button", {
-                                "is-active": editor.isActive("strike")
-                            })}
-                            onClick={toggleStrike}
-                        >
-                            <Icons.Strikethrough />
-                        </button>
-
-                    </div>
-                    <EditorContent editor={editor} />
-
-                </div>
+                <EditorComponent editor={editor}/>
                 <Link to='../travel-teams'>
                     <button type="button" className="btn btn-labeled btn-danger ">
                         <span className="btn-label"><i className="bi bi-x"></i></span>
---
+++ src/pages/dashboard/travel-teams-dashboard/create-travel-team/EditorComponent.tsx
@@ -0,0 +1,159 @@
+import { EditorContent, Editor } from '@tiptap/react'
+import classNames from 'classnames'
+import React, { useCallback } from 'react'
+import * as Icons from "../../Icons";
+
+type EditorProps = {
+    editor: Editor;
+    
+}
+
+const EditorComponent = ({ editor }: {editor: Editor}) => {
+
+    
+    const toggleBold = useCallback(() => {
+        editor.chain().focus().toggleBold().run();
+    }, [editor]);
+
+    const toggleUnderline = useCallback(() => {
+        editor.chain().focus().toggleUnderline().run();
+    }, [editor]);
+
+    const toggleItalic = useCallback(() => {
+        editor.chain().focus().toggleItalic().run();
+    }, [editor]);
+
+    const toggleStrike = useCallback(() => {
+        editor.chain().focus().toggleStrike().run();
+    }, [editor]);
+
+    const toggleParagraph = useCallback(() => {
+        editor.chain().focus().setParagraph().run();
+    }, [editor]);
+
+    const toggleBulletList = useCallback(() => {
+        editor.chain().focus().toggleBulletList().run();
+    }, [editor]);
+
+    const toggleOrderedList = useCallback(() => {
+        editor.chain().focus().toggleOrderedList().run();
+    }, [editor]);
+
+    const toggleH1 = useCallback(() => {
+        editor.chain().focus().toggleHeading({ level: 1 }).run();
+    }, [editor]);
+
+    const toggleH2 = useCallback(() => {
+        editor.chain().focus().toggleHeading({ level: 2 }).run();
+    }, [editor]);
+    
+    const toggleH3 = useCallback(() => {
+        editor.chain().focus().toggleHeading({ level: 3 }).run();
+    }, [editor]);
+
+  return (
+    <div className="editor mb-4">
+                    <div className="menu">
+                        <button
+                            className="menu-button"
+                            onClick={() => editor.chain().focus().undo().run()}
+                            disabled={!editor.can().undo()}
+                        >
+                            <Icons.RotateLeft />
+                        </button>
+                        <button
+                            className="menu-button"
+                            onClick={() => editor.chain().focus().redo().run()}
+                            disabled={!editor.can().redo()}
+                        >
+                            <Icons.RotateRight />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("parapgraph")
+                            })}
+                            onClick={toggleParagraph}
+                        >
+                            <Icons.Paragraph />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("H1")
+                            })}
+                            onClick={toggleH1}
+                        >
+                            <Icons.H1 />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("H2")
+                            })}
+                            onClick={toggleH2}
+                        >
+                            <Icons.H2 />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("H3")
+                            })}
+                            onClick={toggleH3}
+                        >
+                            <Icons.H3 />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("ul")
+                            })}
+                            onClick={toggleBulletList}
+                        >
+                            <Icons.BulletList />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("ol")
+                            })}
+                            onClick={toggleOrderedList}
+                        >
+                            <Icons.OrderedList />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("bold")
+                            })}
+                            onClick={toggleBold}
+                        >
+                            <Icons.Bold />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("underline")
+                            })}
+                            onClick={toggleUnderline}
+                        >
+                            <Icons.Underline />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("intalic")
+                            })}
+                            onClick={toggleItalic}
+                        >
+                            <Icons.Italic />
+                        </button>
+                        <button
+                            className={classNames("menu-button", {
+                                "is-active": editor.isActive("strike")
+                            })}
+                            onClick={toggleStrike}
+                        >
+                            <Icons.Strikethrough />
+                        </button>
+
+                    </div>
+                    <EditorContent editor={editor} />
+
+                </div>
+  )
+}
+
+export default EditorComponent
