afac955674df09303ccfa0aa49e4ca49097ea147
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-06-03 19:39:58 -0400

    Rec leagues now grab data and put in cards

--- src/pages/rec-leagues/RecLeagues.tsx
+++ src/pages/rec-leagues/RecLeagues.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RecLeagues.css'
 import NoEvents from '../../components/helpers/NoEvents'
+import { db } from '../../config/firebase'
+import { getDocs, collection } from 'firebase/firestore'
+import RecLeagueCard from '../../components/helpers/rec-leagues/RecLeagueCard'
 
 const RecLeagues = () => {
+  const [eventList, setEventList] = useState([{}])
+
+  const eventCollectionRef = collection(db, "rec-leagues");
+
+  useEffect(() => {
+    const getEventList = async () => {
+      // READ DATA
+      try {
+        const data = await getDocs(eventCollectionRef)
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setEventList(filteredData)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    getEventList()
+  }, [])
+
+  const showEvents = () => {
+    return (
+      <div className='mt-3'>
+        {/* <span className="display-5 d-block m-3 mx-auto">Rec League Events</span> */}
+        {eventList.map((event: any) => (
+          <RecLeagueCard name={event.name} location={event.location} rules={event.rules} imgSrc={event.imgUrl} />
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div className='main-div'>
-            <NoEvents title='Rec Leagues'/>
+    <div className='main-div p-3 container-fluid'>
+
+      <div>
+        {eventList ? showEvents()
+ : <NoEvents title='Rec Leagues' />}
+      </div>
     </div>
   )
 }
--- src/pages/travel-teams/TravelTeams.tsx
+++ src/pages/travel-teams/TravelTeams.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './TravelTeams.css'
 import NoEvents from '../../components/helpers/NoEvents'
+import TravelTeamCard from '../../components/helpers/travel-teams/TravelTeamCard'
 
 const TravelTeams = () => {
   return (
-    <div className='main-div'>
-            <NoEvents title='Travel Teams'/>
+    <div className='main-div p-3'>
+      <TravelTeamCard/>
+            {/* <NoEvents title='Travel Teams'/> */}
     </div>
   )
 }
