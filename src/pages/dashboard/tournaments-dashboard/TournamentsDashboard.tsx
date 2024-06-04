001055a2e8fc3cd6ad44fc29a3de86e8c7f6b9c5
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-06-03 20:59:09 -0400

    Tournaments now show on dashboard

--- src/pages/dashboard/tournaments-dashboard/TournamentsDashboard.tsx
+++ src/pages/dashboard/tournaments-dashboard/TournamentsDashboard.tsx
@@ -1,9 +1,49 @@
 import { Link } from 'react-router-dom'
 import './TournamentsDashboard.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TournamentCard from '../../../components/helpers/tournaments/TournamentCard'
+import NoEvents from '../../../components/helpers/NoEvents'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../../../config/firebase'
 
 
 const TournamentsDashboard = () => {
+    
+    const [eventList, setEventList] = useState([{}])
+
+    const eventCollectionRef = collection(db, "tournaments");
+  
+    useEffect(() => {
+      const getEventList = async () => {
+        // READ DATA
+        try {
+          const data = await getDocs(eventCollectionRef)
+          const filteredData = data.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }));
+          setEventList(filteredData)
+        } catch (err) {
+          console.log(err)
+        }
+      };
+  
+      getEventList()
+    }, [])
+
+
+    const showEvents = () => {
+        return (
+            <div className='mt-3'>
+                {/* <span className="display-5 d-block m-3 mx-auto">Rec League Events</span> */}
+                {eventList.map((event: any) => (
+                    <TournamentCard name={event.name} location={event.location} rules={event.rules} imgSrc={event.imgUrl} startDate={event.startDate} endDate={event.endDate} />
+                ))}
+            </div>
+        )
+    }
+
+
     return (
         <div>
             <div className='d-grid gap-2 d-md-flex justify-content-md-end '>
@@ -15,10 +55,14 @@ const TournamentsDashboard = () => {
                 </Link>
             </div>
             <div className='card-view overflow-auto'>
-
+                <div>
+                    {eventList ? showEvents()
+                        : <NoEvents title='Rec Leagues' />}
+                </div>
             </div>
         </div>
     )
+    
 }
 
 export default TournamentsDashboard
