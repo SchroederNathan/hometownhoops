041e19167e0a88aa23b5cd1ff5291919682e524b
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-06-03 21:29:44 -0400

    All heroes are finished on therespective landing pages

--- src/pages/tournaments/Tournaments.tsx
+++ src/pages/tournaments/Tournaments.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Tournaments.css'
 import NoEvents from '../../components/helpers/NoEvents'
+import TournamentCard from '../../components/helpers/tournaments/TournamentCard'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../../config/firebase'
 
 const Tournaments = () => {
+  const [eventList, setEventList] = useState([{}])
+
+  const eventCollectionRef = collection(db, "tournaments");
+
+  useEffect(() => {
+    const getEventList = async () => {
+      // READ DATA
+      try {
+        const data = await getDocs(eventCollectionRef)
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setEventList(filteredData)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    getEventList()
+  }, [])
+
+  const showEvents = () => {
+    return (
+      <div className='mt-3'>
+        {/* <span className="h1 d-block m-3 mb-4 text-center"><strong>Active Rec Leagues</strong></span> */}
+
+        <span className="h1 d-block m-3 mb-2 text-center"><strong>Tournaments</strong></span>
+        <p className='w-75 text-center mx-auto mb-5 lead'>Find information about upcoming basketball tournaments for players of all skill levels. Our tournaments offer a competitive and fun environment to improve skills and connect with others in the community. We offer a variety of tournaments, including youth, adult, cash prize, and 4-on-4 tournaments. Don't miss out on the chance to register and join the action!</p>
+        {eventList.map((event: any) => (
+          <TournamentCard name={event.name} location={event.location} rules={event.rules} imgSrc={event.imgUrl} startDate={event.startDate} endDate={event.endDate} />
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div className='main-div'>
-      <NoEvents title='Tournaments'/>
+    <div className='main-div p-3 container-fluid'>
+
+      <div>
+        {eventList ? showEvents()
+          : <NoEvents title='Tournaments' />}
+      </div>
     </div>
   )
 }
--- src/pages/travel-teams/TravelTeams.tsx
+++ src/pages/travel-teams/TravelTeams.tsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './TravelTeams.css'
 import NoEvents from '../../components/helpers/NoEvents'
 import TravelTeamCard from '../../components/helpers/travel-teams/TravelTeamCard'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../../config/firebase'
 
 const TravelTeams = () => {
+  const [eventList, setEventList] = useState([{}])
+
+  const eventCollectionRef = collection(db, "travel-teams");
+
+  useEffect(() => {
+    const getEventList = async () => {
+      // READ DATA
+      try {
+        const data = await getDocs(eventCollectionRef)
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setEventList(filteredData)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    getEventList()
+  }, [])
+
+  const showEvents = () => {
+    return (
+      <div className='mt-3'>
+        {/* <span className="h1 d-block m-3 mb-4 text-center"><strong>Active Rec Leagues</strong></span> */}
+
+        <span className="h1 d-block m-3 mb-2 text-center"><strong>Travel Teams</strong></span>
+        <p className='w-75 text-center mx-auto mb-5 lead'>
+          At Hometown Hoops, we're not just about basketball -
+          we're about building better players and better people both on
+          and off the court. Our travel teams are where competitive spirit
+          meets personal growth. Led by dedicated coaches and staff, we
+          focus on improving skills, fostering teamwork, and promoting
+          leadership qualities in our players. Whether you're just starting
+          out or looking for more exposure, our program is designed to help
+          you reach your full potential!
+        </p>
+        {eventList.map((event: any) => (
+          <TravelTeamCard name={event.name} location={event.location} rules={event.rules} imgSrc={event.imgUrl} startDate={event.startDate} endDate={event.endDate} />
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div className='main-div p-3'>
-      <TravelTeamCard/>
-            {/* <NoEvents title='Travel Teams'/> */}
+    <div className='main-div p-3 container-fluid'>
+
+      <div>
+        {eventList ? showEvents()
+          : <NoEvents title='travel-teams' />}
+      </div>
     </div>
   )
 }
