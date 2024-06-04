c36a6586523b9b9909ec4ade1792abb526cc338b
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-06-03 20:49:25 -0400

    Can now create from trael team preview

--- src/pages/dashboard/travel-teams-dashboard/create-travel-team/preview/PreviewCreateTravelTeam.tsx
+++ src/pages/dashboard/travel-teams-dashboard/create-travel-team/preview/PreviewCreateTravelTeam.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './PreviewCreateTravelTeam.css'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import TravelTeamCard from '../../../../../components/helpers/travel-teams/TravelTeamCard';
+import { db } from '../../../../../config/firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 
 
@@ -14,6 +16,25 @@ const PreviewCreateTravelTeam = () => {
     const startDate = state.startDate;
     const endDate = state.endDate;
     const rules = state.rules;
+
+    const eventsCollectionRef = collection(db, 'rec-leagues')
+
+    const onCreate = async () => {
+        try {
+            await addDoc(eventsCollectionRef, {
+                name: name,
+                location: location,
+                startDate: startDate,
+                endDate: endDate,
+                imgUrl: 'none',
+                rules: rules
+            })
+            navigate("/dashboard/rec-leagues/")
+        } catch (err) {
+            alert(err)
+        }
+
+    }
 
     const navigate = useNavigate();
 
@@ -52,7 +73,7 @@ const PreviewCreateTravelTeam = () => {
                     Cancel
                 </button>
             </Link>
-            <button type="button" className="btn btn-labeled-1 btn-primary float-end create-button">
+            <button type="button" className="btn btn-labeled-1 btn-primary float-end create-button" onClick={onCreate}>
                 Create
                 <span className="btn-label-1"><i className="bi bi-check"></i></span>
             </button>
