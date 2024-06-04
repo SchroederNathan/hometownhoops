b5baa15d35aa57f76dcb7a2a169f0e7b7f8c4cd0
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-06-03 21:23:28 -0400

    Adjusted UI on rec league home page

--- src/pages/rec-leagues/RecLeagues.tsx
+++ src/pages/rec-leagues/RecLeagues.tsx
@@ -31,9 +31,10 @@ const RecLeagues = () => {
   const showEvents = () => {
     return (
       <div className='mt-3'>
-        <span className="display-5 d-block m-3 mb-4 text-center"><strong>Active Rec Leagues</strong></span>
+        {/* <span className="h1 d-block m-3 mb-4 text-center"><strong>Active Rec Leagues</strong></span> */}
 
-        <span className="display-5 d-block m-3 mb-4 text-center"><strong>Open Registration</strong></span>
+        <span className="h1 d-block m-3 mb-2 text-center"><strong>Rec Leagues</strong></span>
+        <p className='w-75 text-center mx-auto mb-5 lead'>Hometown Hoops Rec Basketball Leagues offer a unique opportunity for basketball lovers of all skill levels to stay active, connect with others, and have fun on the court. Our leagues provide a competitive yet welcoming environment that emphasizes community building and sportsmanship. Join us for a season of fun and competition with Hometown Hoops Rec Basketball Leagues!</p>
         {eventList.map((event: any) => (
           <RecLeagueCard name={event.name} location={event.location} rules={event.rules} imgSrc={event.imgUrl} startDate={event.startDate} endDate={event.endDate} />
         ))}
