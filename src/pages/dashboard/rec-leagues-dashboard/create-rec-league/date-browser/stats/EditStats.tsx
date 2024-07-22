f4e3fcecbf974fe22f47b226bb8189b907cf4af9
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-07-21 23:32:06 -0400

    Adjusted aggrid layout

--- src/pages/dashboard/rec-leagues-dashboard/create-rec-league/date-browser/stats/EditStats.tsx
+++ src/pages/dashboard/rec-leagues-dashboard/create-rec-league/date-browser/stats/EditStats.tsx
@@ -10,46 +10,47 @@ ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 // Create new GridExample component
 const StatsGrid = () => {
-  // Row Data: The data to be displayed.
-  const [teamRowData, setTeamRowData] = useState<Team[]>([
-    { name: "Tesla", wins: 1, losses: 2 },
-    { name: "Monstars", wins: 1, losses: 2 },
-    { name: "Test Data", wins: 3, losses: 2 },
-    { name: "Anotha", wins: 1, losses: 2 },
-  ]);
+    // Row Data: The data to be displayed.
+    const [teamRowData, setTeamRowData] = useState<Team[]>([
+        { name: "Tesla", wins: 1, losses: 2 },
+        { name: "Monstars", wins: 1, losses: 2 },
+    ]);
 
-  // Column Definitions: Defines & controls grid columns.
-  const [teamColDefs, setTeamColDefs] = useState<ColDef<Team>[]>([
-    { field: "name", flex: 2 },
-    { field: "wins" , flex: 1},
-    { field: "losses", flex: 1},
-  ]);
+    // Column Definitions: Defines & controls grid columns.
+    const [teamColDefs, setTeamColDefs] = useState<ColDef<Team>[]>([
+        { field: "name", flex: 2 },
+        { field: "wins", flex: 1 },
+        { field: "losses", flex: 1 },
+    ]);
 
-  const defaultColDef: ColDef = {
-    flex: 1,
-  };
+    const defaultColDef: ColDef = {
+        flex: 1,
+    };
 
-  // Container: Defines the grid's theme & dimensions.
-  return (
-    <div
-      className={"ag-theme-quartz"}
-      style={{ width: "100%", height: "200px" }}
-    >
-      <AgGridReact
-        rowData={teamRowData}
-        columnDefs={teamColDefs}
-        defaultColDef={defaultColDef}
-      />
-    </div>
-  );
+    // Container: Defines the grid's theme & dimensions.
+    return (
+        <div
+            className={"ag-theme-quartz"}
+            style={{ width: "100%" }}
+        >
+            <AgGridReact
+                rowData={teamRowData}
+                columnDefs={teamColDefs}
+                rowSelection={"single"}
+                domLayout={"autoHeight"}
+
+                defaultColDef={defaultColDef}
+            />
+        </div>
+    );
 };
 
 const EditStats = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <StatsGrid />
+        </div>
+    )
 }
 
 export default EditStats
--- src/pages/dashboard/rec-leagues-dashboard/create-rec-league/date-browser/stats/StatsModal.tsx
+++ src/pages/dashboard/rec-leagues-dashboard/create-rec-league/date-browser/stats/StatsModal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import { Team } from '../../teams/TeamsCreateRecLeague';
 import { Game } from '../DateBrowser';
+import EditStats from './EditStats';
 
 interface GameModalProps {
     show: boolean;
@@ -58,39 +59,10 @@ const StatsModal: React.FC<GameModalProps> = ({
                 <Modal.Title>Edit Stats</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                {/* <label className="form-label mb-3 fw-bold">{selectedDate.toDateString()}</label> */}
-                {/* <Form.Group className="mb-3">
-                    <Form.Label>Away Team</Form.Label>
-                    <Form.Select
-                        value={awayTeam}
-                        onChange={(e) => setAwayTeam(e.target.value)}
-                    >
-                        <option value="">Select Away Team</option>
-                        {teams.map((team) => (
-                            <option key={team.id} value={team.name}>{team.name}</option>
-                        ))}
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label>Home Team</Form.Label>
-                    <Form.Select
-                        value={homeTeam}
-                        onChange={(e) => setHomeTeam(e.target.value)}
-                    >
-                        <option value="">Select Home Team</option>
-                        {teams.map((team) => (
-                            <option key={team.id} value={team.name}>{team.name}</option>
-                        ))}
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label>Time</Form.Label>
-                    <Form.Control
-                        type="time"
-                        value={time}
-                        onChange={(e) => setTime(e.target.value)}
-                    />
-                </Form.Group> */}
+                 <label className="form-label mb-3 fw-bold">Teams</label> 
+
+                <EditStats />
+                <label className="form-label mb-3 mt-3 fw-bold">Players</label> 
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={onHide}>
