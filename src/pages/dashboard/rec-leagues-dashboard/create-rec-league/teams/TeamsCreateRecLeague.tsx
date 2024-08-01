75f878daafed7c114afd0612261d2861a8bc90ba
“Nathan <schroedernathan2011@icloud.com>
2024-08-01 18:46:35 -0400

    Added new landing page

--- src/pages/dashboard/rec-leagues-dashboard/create-rec-league/teams/TeamsCreateRecLeague.tsx
+++ src/pages/dashboard/rec-leagues-dashboard/create-rec-league/teams/TeamsCreateRecLeague.tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import TeamsModal from "./TeamsModal";
 import { v4 as uuidv4 } from "uuid";
-import { doc, writeBatch, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  writeBatch,
+  getDoc,
+  getDocs,
+  collection,
+} from "firebase/firestore";
 import { db } from "../../../../../config/firebase";
 
 interface Player {
@@ -40,10 +46,61 @@ const TeamsCreateRecLeague: React.FC<TeamsCreateRecLeagueProps> = ({
   const rules = state.rules;
   const games = state.games || [];
 
+  const [loading, setLoading] = useState(true);
   const [modalShow, setModalShow] = useState(false);
-  const [teams, setTeams] = useState<Team[]>(state.teams || []);
+  const [teams, setTeams] = useState<Team[]>([]);
   const [teamToEdit, setTeamToEdit] = useState<Team | null>(null);
   const [teamsToDelete, setTeamsToDelete] = useState<string[]>([]);
+
+  // Function to fetch teams data if not provided in state
+  const fetchTeamsData = async () => {
+    try {
+      const teamsCollectionRef = collection(
+        db,
+        "rec-leagues",
+        eventID,
+        "teams"
+      );
+      const teamsSnapshot = await getDocs(teamsCollectionRef);
+      const teamsList: Team[] = [];
+
+      for (const teamDoc of teamsSnapshot.docs) {
+        const teamData = teamDoc.data() as Team;
+        const playersCollectionRef = collection(teamDoc.ref, "players");
+        const playersSnapshot = await getDocs(playersCollectionRef);
+
+        const playersList: Player[] = playersSnapshot.docs.map((playerDoc) => ({
+          id: playerDoc.id,
+          ...(playerDoc.data() as Player),
+        }));
+
+        const teamWithPlayers: Team = {
+          id: teamDoc.id,
+          ...teamData,
+          players: playersList,
+        };
+
+        teamsList.push(teamWithPlayers);
+      }
+
+      setTeams(teamsList);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching teams data: ", error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (state && state.teams) {
+      // If teams are passed in state, use them
+      setTeams(state.teams);
+      setLoading(false);
+    } else {
+      // Otherwise, fetch from database
+      fetchTeamsData();
+    }
+  }, [state]);
 
   const preview = (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -52,7 +109,16 @@ const TeamsCreateRecLeague: React.FC<TeamsCreateRecLeagueProps> = ({
     event.preventDefault();
     if (tabName === "info") {
       navigate("/dashboard/rec-leagues/create", {
-        state: { name, location, deadline, startDate, endDate, rules, teams, games },
+        state: {
+          name,
+          location,
+          deadline,
+          startDate,
+          endDate,
+          rules,
+          teams,
+          games,
+        },
       });
     } else if (tabName === "preview") {
       navigate("/dashboard/rec-leagues/create/preview", {
@@ -157,128 +223,138 @@ const TeamsCreateRecLeague: React.FC<TeamsCreateRecLeagueProps> = ({
 
   return (
     <div>
-      {isEditing ? (
-        <ul className="nav nav-tabs mb-3">
-          <li className="nav-item" style={{ marginRight: "2px" }}>
-            <a
-              className="nav-link tab activeTab"
-              onClick={(event) => preview(event, "edit")}
-              href="#"
-            >
-              Information
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active tab" href="#">
-              Teams
-            </a>
-          </li>
-        </ul>
+      {loading ? (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
       ) : (
-        <ul className="nav nav-tabs mb-3">
-          <li className="nav-item" style={{ marginRight: "2px" }}>
-            <a
-              className="nav-link tab activeTab"
-              onClick={(event) => preview(event, "info")}
-              href="#"
-            >
-              Information
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active tab" href="#">
-              Teams
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link tab"
-              onClick={(event) => preview(event, "preview")}
-              href="#"
-            >
-              Preview
-            </a>
-          </li>
-        </ul>
-      )}
+        <>
+          {isEditing ? (
+            <ul className="nav nav-tabs mb-3">
+              <li className="nav-item" style={{ marginRight: "2px" }}>
+                <a
+                  className="nav-link tab activeTab"
+                  onClick={(event) => preview(event, "edit")}
+                  href="#"
+                >
+                  Information
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active tab" href="#">
+                  Teams
+                </a>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav nav-tabs mb-3">
+              <li className="nav-item" style={{ marginRight: "2px" }}>
+                <a
+                  className="nav-link tab activeTab"
+                  onClick={(event) => preview(event, "info")}
+                  href="#"
+                >
+                  Information
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active tab" href="#">
+                  Teams
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link tab"
+                  onClick={(event) => preview(event, "preview")}
+                  href="#"
+                >
+                  Preview
+                </a>
+              </li>
+            </ul>
+          )}
 
-      <button
-        type="button"
-        className="btn btn-teams"
-        onClick={() => setModalShow(true)}
-      >
-        <span className="btn-label-teams">
-          <i className="bi bi-plus"></i>
-        </span>
-        Create Team
-      </button>
-      <TeamsModal
-        show={modalShow}
-        onHide={() => {
-          setModalShow(false);
-          setTeamToEdit(null);
-        }}
-        parentCallback={addOrEditTeam}
-        teamToEdit={teamToEdit}
-      />
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Team Name</th>
-            <th scope="col" className="text-center">
-              Player Count
-            </th>
-            <th scope="col" className="text-center">
-              Edit Players
-            </th>
-            <th scope="col" className="text-center">
-              Remove Team
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {teams.map((team) => (
-            <tr key={team.id}>
-              <td>{team.name}</td>
-              <td className="text-center">{team.players.length}</td>
-              <td scope="row" className="text-center">
-                <div className="d-flex justify-content-center">
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => editTeam(team)}
-                  >
-                    Edit Team
-                  </button>
-                </div>
-              </td>
-              <td scope="row" className="text-center">
-                <div className="d-flex justify-content-center">
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => removeTeam(team.id)}
-                  >
-                    <span>
-                      <i className="bi bi-x"></i>
-                    </span>
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {isEditing ? (
-        <button
-          type="button"
-          className="btn btn-labeled-1 btn-primary float-end create-button mt-3"
-          onClick={onUpdate}
-        >
-          Update
-          <span className="btn-label-1">
-            <i className="bi bi-check"></i>
-          </span>
-        </button>
-      ) : null}
+          <button
+            type="button"
+            className="btn btn-teams"
+            onClick={() => setModalShow(true)}
+          >
+            <span className="btn-label-teams">
+              <i className="bi bi-plus"></i>
+            </span>
+            Create Team
+          </button>
+          <TeamsModal
+            show={modalShow}
+            onHide={() => {
+              setModalShow(false);
+              setTeamToEdit(null);
+            }}
+            parentCallback={addOrEditTeam}
+            teamToEdit={teamToEdit}
+          />
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Team Name</th>
+                <th scope="col" className="text-center">
+                  Player Count
+                </th>
+                <th scope="col" className="text-center">
+                  Edit Players
+                </th>
+                <th scope="col" className="text-center">
+                  Remove Team
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {teams.map((team) => (
+                <tr key={team.id}>
+                  <td>{team.name}</td>
+                  <td className="text-center">{team.players.length}</td>
+                  <td scope="row" className="text-center">
+                    <div className="d-flex justify-content-center">
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => editTeam(team)}
+                      >
+                        Edit Team
+                      </button>
+                    </div>
+                  </td>
+                  <td scope="row" className="text-center">
+                    <div className="d-flex justify-content-center">
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => removeTeam(team.id)}
+                      >
+                        <span>
+                          <i className="bi bi-x"></i>
+                        </span>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {isEditing ? (
+            <button
+              type="button"
+              className="btn btn-labeled-1 btn-primary float-end create-button mt-3"
+              onClick={onUpdate}
+            >
+              Update
+              <span className="btn-label-1">
+                <i className="bi bi-check"></i>
+              </span>
+            </button>
+          ) : null}
+        </>
+      )}
     </div>
   );
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Button, Card, Carousel, Col, Row } from 'react-bootstrap';
-import './Home.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Card, Carousel, Col, Row } from "react-bootstrap";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 interface Features {
   title: string;
@@ -11,104 +11,200 @@ interface Features {
 }
 
 const Home = () => {
-
-  const featureCards: Features[] = [{
-    title: 'Tournaments',
-    description: 'Discover all the details about our upcoming basketball tournaments',
-    imageURL: 'src/assets/tournaments.jpg',
-    routerURL: '/tournaments'
-  },
-  {
-    title: 'Travel Teams',
-    description: 'Discover how our travel teams can help develop your basketball skills and grow as a player',
-    imageURL: 'src/assets/camps.jpg',
-    routerURL: '/travel-teams'
-  },
-  {
-    title: 'Rec Leagues',
-    description: 'Explore the fun and competitive world of our adult rec basketball leagues',
-    imageURL: 'src/assets/recleague.jpg',
-    routerURL: '/rec-leagues'
-  }]
-
+  const featureCards: Features[] = [
+    {
+      title: "Tournaments",
+      description:
+        "Discover all the details about our upcoming basketball tournaments",
+      imageURL: "src/assets/tournaments.jpg",
+      routerURL: "/tournaments",
+    },
+    {
+      title: "Travel Teams",
+      description:
+        "Discover how our travel teams can help develop your basketball skills and grow as a player",
+      imageURL: "src/assets/camps.jpg",
+      routerURL: "/travel-teams",
+    },
+    {
+      title: "Rec Leagues",
+      description:
+        "Explore the fun and competitive world of our adult rec basketball leagues",
+      imageURL: "src/assets/recleague.jpg",
+      routerURL: "/rec-leagues",
+    },
+  ];
 
   return (
-    <div className='main-div'>
-      <div className='carosel' style={{ display: 'block', maxWidth: '100%' }}>
-        <Carousel>
-          <Carousel.Item className='item' interval={3000}>
-            <img
-              className="d-block w-100"
-              src="src/assets/tournaments.jpg"
-              alt="Image One"
+    <div className="main-div ">
+      <div className="container-fluid landing-container d-flex align-items-center">
+        <div>
+          <h1 className="display-3 fw-bold">Hometown Hoops</h1>
+          <p className="mb-4 lead landing-text">
+            A local organization bringing basketball to people of all ages and
+            skill levels. 
+          </p>
+          <div className="input-group newsletter">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              aria-label="User's Email"
+              aria-describedby="email"
             />
-            <Carousel.Caption>
-              <h3>Label for first slide</h3>
-              <p>Sample Text for Image One</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+            <button
+              className="btn btn-primary newsletter-button"
+              type="button"
+              id="email"
+            >
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
-      <Row xs={1} md={3} className="g-3 cards" style={{ margin: 'auto', maxWidth: '80%', height: 'fit', paddingTop: '20px' }}>
+
+      <Row
+        xs={1}
+        md={3}
+        className="g-3 cards"
+        style={{
+          margin: "auto",
+          maxWidth: "80%",
+          height: "fit",
+        }}
+      >
         {featureCards &&
           featureCards.map((card) => {
-            const { title, description, imageURL, routerURL } =
-              card;
+            const { title, description, imageURL, routerURL } = card;
             return (
               <Col>
-                <Link to={routerURL} style={{textDecoration: 'none'}}>
-                  <Card className="flex-fill shadow card" style={{ width: '100%', height: '25rem', textAlign: 'center', margin: 'auto' }}>
-                    <Card.Img variant="top" style={{ minHeight: '200px', maxHeight: '200px', objectFit: 'cover' }} src={imageURL} />
-                    <Card.Body style={{ overflowY: 'hidden' }}>
+                <Link to={routerURL} style={{ textDecoration: "none" }}>
+                  <Card
+                    className="flex-fill shadow card"
+                    style={{
+                      width: "100%",
+                      height: "25rem",
+                      textAlign: "center",
+                      margin: "auto",
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      style={{
+                        minHeight: "200px",
+                        maxHeight: "200px",
+                        objectFit: "cover",
+                      }}
+                      src={imageURL}
+                    />
+                    <Card.Body style={{ overflowY: "hidden" }}>
                       <Card.Title>{title}</Card.Title>
                       <Card.Text>{description}</Card.Text>
                     </Card.Body>
-                    <Button variant="primary" style={{ margin: '10px' }}>Find {title}</Button>
-
+                    <Button variant="primary" style={{ margin: "10px" }}>
+                      Find {title}
+                    </Button>
                   </Card>
                 </Link>
               </Col>
             );
           })}
       </Row>
-      <div id='about-us' style={{ maxWidth: '80%', margin: 'auto', padding: '20px' }}>
+      <div
+        id="about-us"
+        style={{ maxWidth: "80%", margin: "auto", padding: "20px" }}
+      >
         <hr className="featurette-divider" />
       </div>
-      <div className="row featurette feature" style={{ width: '70%', maxWidth: '1200px', margin: 'auto', display: 'flex', alignItems: 'center' }}>
+      <div
+        className="row featurette feature"
+        style={{
+          width: "70%",
+          maxWidth: "1200px",
+          margin: "auto",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <div className="col-md-7">
           <h2 className="featurette-heading fw-normal lh-1">About Us</h2>
-          <p className="lead">Hometown Hoops was founded by Derek Bedal who saw a need
-            for more opportunities to play basketball in the Leamington and surrounding areas.
-            His idea started with a tournament in 2021. This idea then grew to not only host tournaments
-            but to also include a recreational summer league for young adults the next summer.
-            Derek plans to grow his business to include skill development camps for elementary
-            and high school age players. Derek’s love of basketball began as a young boy and
-            grew as he competed on multiple levels in high school. During this time he spent many
-            summers attending various basketball camps such as PGC, Breakthrough Basketball, and others.
-            Derek attended these camps to constantly grow his basketball IQ and his basketball skills.
-            He hopes his love of the game will spread throughout the local communities as he provides multiple opportunities.</p>
+          <p className="lead">
+            Hometown Hoops was founded by Derek Bedal who saw a need for more
+            opportunities to play basketball in the Leamington and surrounding
+            areas. His idea started with a tournament in 2021. This idea then
+            grew to not only host tournaments but to also include a recreational
+            summer league for young adults the next summer. Derek plans to grow
+            his business to include skill development camps for elementary and
+            high school age players. Derek’s love of basketball began as a young
+            boy and grew as he competed on multiple levels in high school.
+            During this time he spent many summers attending various basketball
+            camps such as PGC, Breakthrough Basketball, and others. Derek
+            attended these camps to constantly grow his basketball IQ and his
+            basketball skills. He hopes his love of the game will spread
+            throughout the local communities as he provides multiple
+            opportunities.
+          </p>
         </div>
         <div className="col-md-5">
-          <img className="shadow-sm bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style={{ minHeight: '500px', objectFit: 'cover' }} height="500" role="img" src='src/assets/portrait.jpg' ></img>
+          <img
+            className="shadow-sm bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ minHeight: "500px", objectFit: "cover" }}
+            height="500"
+            role="img"
+            src="src/assets/portrait.jpg"
+          ></img>
         </div>
       </div>
 
-      <div style={{ maxWidth: '80%', margin: 'auto', padding: '10px' }}>
+      <div style={{ maxWidth: "80%", margin: "auto", padding: "10px" }}>
         <hr className="featurette-divider" />
       </div>
 
-      <h2 style={{ width: '70%', margin: 'auto', textAlign: 'center' }}>Our Sponsor's</h2>
-      <p className="text-body-secondary" style={{ width: '70%', margin: 'auto', textAlign: 'center' }}>We are incredibly grateful for the support of our sponsor's that help make Hometown Hoops a success.</p>
+      <h2 style={{ width: "70%", margin: "auto", textAlign: "center" }}>
+        Our Sponsor's
+      </h2>
+      <p
+        className="text-body-secondary"
+        style={{ width: "70%", margin: "auto", textAlign: "center" }}
+      >
+        We are incredibly grateful for the support of our sponsor's that help
+        make Hometown Hoops a success.
+      </p>
 
-      <div style={{ width: '70%', margin: 'auto', textAlign: 'center' }}>
-        <img src='src/assets/sponsor1.png' width={200} height={200} style={{ padding: '10px' }} />
-        <img src='src/assets/sponsor2.png' width={200} height={200} style={{ padding: '10px' }} />
-        <img src='src/assets/sponsor3.png' width={200} height={200} style={{ padding: '10px' }} />
-        <img src='src/assets/sponsor4.png' width={200} height={200} style={{ padding: '10px' }} />
-        <img src='src/assets/sponsor5.png' width={200} height={200} style={{ padding: '10px' }} />
+      <div style={{ width: "70%", margin: "auto", textAlign: "center" }}>
+        <img
+          src="src/assets/sponsor1.png"
+          width={200}
+          height={200}
+          style={{ padding: "10px" }}
+        />
+        <img
+          src="src/assets/sponsor2.png"
+          width={200}
+          height={200}
+          style={{ padding: "10px" }}
+        />
+        <img
+          src="src/assets/sponsor3.png"
+          width={200}
+          height={200}
+          style={{ padding: "10px" }}
+        />
+        <img
+          src="src/assets/sponsor4.png"
+          width={200}
+          height={200}
+          style={{ padding: "10px" }}
+        />
+        <img
+          src="src/assets/sponsor5.png"
+          width={200}
+          height={200}
+          style={{ padding: "10px" }}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
